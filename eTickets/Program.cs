174122b4ca008e2b05b975fcf51a40b6916a2093
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2023-04-15 19:07:55 +0300

    ActorsService added

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString
     ("DefaultConnectionString")));
 
+//Services configuration
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 var app = builder.Build();
 

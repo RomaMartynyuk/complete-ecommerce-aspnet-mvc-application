210a1ed650227fc03a805681cf7769dd6fc7c1f4
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2023-04-18 18:41:13 +0300

    Entity Base Repository
    Added EntityBase interface with id, IEntityBaseRepository and EntityBaseRepository with all methods (GetAllAsync, GetByIdAsync, AddAsync. UpdateAsync, DeleteAsync) and configurated them to Actors

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context;
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Add method
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        //Delete method
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
+
+            await _context.SaveChangesAsync();
+        }
+
+        //Get all method
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+        //Get by id method
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+
+        //Edit method
+        public async Task UpdateAsync(int id, T entity)
+        {
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}

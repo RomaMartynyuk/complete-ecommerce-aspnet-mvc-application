39583f5e46edf9749a0c04e079d66375918e7cf8
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2023-04-23 22:32:22 +0300

    Create, Update and Search for Movies
    added create, update and search method for movies

---
+++ eTickets/Data/ViewModel/NewMovieVM.cs
@@ -0,0 +1,52 @@
+﻿using eTickets.Data.Base;
+using eTickets.Data.enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class NewMovieVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Movie name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Movie description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        [Display(Name = "Movie price")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Movie poster")]
+        [Required(ErrorMessage = "Poster is required")]
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Movie start date")]
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Movie end date")]
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Movie category")]
+        [Required(ErrorMessage = "Movie category is required")]
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationship
+        [Display(Name = "Select actor(s)")]
+        [Required(ErrorMessage = "Movie actor(s) is required")]
+        public List<int> ActorIds { get; set; }
+
+        [Display(Name = "Select a cinema")]
+        [Required(ErrorMessage = "Movie cinema is required")]
+        public int CinemaId { get; set; }
+
+        [Display(Name = "Select a producer")]
+        [Required(ErrorMessage = "Movie producer is required")]
+        public int ProducerId { get; set; }
+    }
+}

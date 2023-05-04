15ce4d9928d12f47b96b8a34f34eb001cd95b49d
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2023-05-04 21:33:01 +0300

    SignUp and LogOut methods

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Static;
 using eTickets.Data.ViewModel;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
 
 namespace eTickets.Controllers
 {
@@ -42,11 +44,54 @@ namespace eTickets.Controllers
                         return RedirectToAction("Index", "Movies");
                     }
                 }
-                TempData["Error"] = "Wromg credentials. Please, try again!";
+                TempData["Error"] = "Wrong credentials. Please, try again!";
                 return View(loginVM);
             }
-            TempData["Error"] = "Wromg credentials. Please, try again!";
+            TempData["Error"] = "Wrong credentials. Please, try again!";
             return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if(user != null)
+            {
+                TempData["Error"] = "This email address is already use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+
+            return View("RegisterCompleted");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
         }
     }
 }

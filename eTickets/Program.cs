49272cc5231d41fd9bccb60ed1cd97ef3eed6855
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2023-05-12 22:40:48 +0300

    azure deployements 2

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -61,6 +61,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 
 app.Run();

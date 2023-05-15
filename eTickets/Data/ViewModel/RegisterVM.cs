baac9f5e038cc3d326ed71449952dfca96986dbe
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2023-05-15 15:42:22 +0300

    Password fix

--- eTickets/Data/ViewModel/RegisterVM.cs
+++ eTickets/Data/ViewModel/RegisterVM.cs
@@ -14,6 +14,8 @@ namespace eTickets.Data.ViewModel
 
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Password must be at least 8 characters and contain upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         public string Password { get; set; }
 
         [Display(Name = "Confirm password")]
